6090b19b68b52ec4b81e7dc09621d7b0010c8b04
liambarry7 <74929060+liambarry7@users.noreply.github.com>
2025-11-19 21:38:59 +0000

    Created first basic pages

---
+++ disaster_advice.js
@@ -0,0 +1,28 @@
+// Load the navigation bar
+const navBar = document.querySelector("header");
+function loadHeader() {
+    let displayContent =
+    `
+        <nav class="nav_bar">
+            <ul>
+                <input type="text" placeholder="Search..">
+                <li class="navigation"><a href="index.html">Home</a></li>
+                <li class="navigation active"><a href="disaster_advice.html">Disaster Advice</a></li>
+                <li class="navigation"><a href="ema.html">Emergancy Medical Advice</a></li>
+                <li class="navigation"><a href="regional_info.html">Region Info</a></li>
+            </ul>
+        </nav>
+    `;
+
+    navBar.innerHTML = displayContent;
+}
+
+window.addEventListener("DOMContentLoaded", loadHeader, false);
+
+// Load page content
+const infoSection = document.querySelector("#info-section");
+function loadContent() {
+    
+}
+
+window.addEventListener("DOMContentLoaded", loadContent, false);
