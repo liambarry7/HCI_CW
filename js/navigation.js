30b1182d1e84a708b582178423d5e995b578d59c
liambarry7 <74929060+liambarry7@users.noreply.github.com>
2025-11-22 00:45:53 +0000

    Begun css for nav bar

--- js/navigation.js
+++ js/navigation.js
@@ -3,14 +3,18 @@ const navBar = document.getElementById("navBar");
 function loadHeader() {
     let displayContent =
     `
-        <nav>
-            <ul>
-                <input type="text" placeholder="Search..">
+        <nav id="nav">
+            <ul id="navList">
+                <li class="navigation"><input id="searchBar" type="text" placeholder="Search.."></li>
                 <li class="navigation"><a href="index.html">Home</a></li>
                 <li class="navigation active"><a href="disaster_advice.html">Disaster Advice</a></li>
                 <li class="navigation"><a href="ema.html">Emergancy Medical Advice</a></li>
                 <li class="navigation"><a href="regional_info.html">Region Info</a></li>
-                <input type="image" src="images/settings.png    ">
+                <li class="navigation">
+                    <button id="settingsButton">
+                        <img id="settingsIcon" src="images/settings.png">
+                    </button>
+                </li>
             </ul>
         </nav>
     `;
