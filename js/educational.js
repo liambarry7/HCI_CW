09c0da6c524105063b6ee07bdcebf324cbb3dba7
liambarry7 <liambarry406@yahoo.co.uk>
2025-12-04 15:06:50 +0000

    functionallity of quiz complete
    JS functions implemented and basic css classes/ids ready for changing

--- js/educational.js
+++ js/educational.js
@@ -15,48 +15,80 @@ const questions = [
         question: "Who won the 2006 F1 World Championship?",
         options: ["Kimi Raikkonen", "Jenson Button", "Fernando Alonso", "Giancarlo Fisichella"],
         answer: "c"
+    },
+
+    {
+        question: "Which driver raced for lotus in 2012?",
+        options: ["Nico Hulkenburg", "Nico Rosberg", "Timo Glock", "Romain Grosjean"],
+        answer: "d"
+    },
+
+    {
+        question: "Who won the 2012 Spanish GP?",
+        options: ["Fernando Alonso", "Pastor Maldonado", "Sebastian Vettel", "Lewis Hamilton"],
+        answer: "b"
+    },
+
+    {
+        question: "Who is the only team to win the constructors championship their debut season?",
+        options: ["Ferrari", "Williams", "Mclaren", "Brawn GP"],
+        answer: "d"
+    },
+
+    {
+        question: "Which driver has 32 Victories?",
+        options: ["Fernando Alonso", "Sterling Moss", "Niki Lauda", "James Hunt"],
+        answer: "a"
+    },
+
+    {
+        question: "Who won the 2023 conference league final?",
+        options: ["Roma", "Fiorientina", "West Ham United", "Olympiacos"],
+        answer: "c"
     }
 ];
-
-
-const quiz = document.getElementById("quiz");
-
-function loadQuizQuestion() {
-    quiz.innerHTML = `
-        <div id="quiz">
-            <h3 id="questionTitle">${questions[questionCount].question}</h3>
-            
-            <div id="answerSet">
-                <div class="answerBox">
-                    <input type="radio" id="a" name="disasterQuiz" value="${questions[questionCount].options[0]}">
-                    <label for="a">${questions[questionCount].options[0]}</label>
-                </div>
-                <div class="answerBox">
-                    <input type="radio" id="b" name="disasterQuiz" value="${questions[questionCount].options[1]}">
-                    <label for="b">${questions[questionCount].options[1]}</label>
-                </div>
-                <div class="answerBox">
-                    <input type="radio" id="c" name="disasterQuiz" value="${questions[questionCount].options[2]}">
-                    <label for="c">${questions[questionCount].options[2]}</label>
-                </div>
-                <div class="answerBox">
-                    <input type="radio" id="d" name="disasterQuiz" value="${questions[questionCount].options[3]}">
-                    <label for="d">${questions[questionCount].options[3]}</label>
-                </div>
-                <p class="quizError">Please choose an answer</p>
-                <button type="submit" onclick="myFunction()" id="quizSubmit" class="optionSubmit">Submit</button>
-
-            </div>
-        </div>
-        `;               
-}
-
-window.addEventListener("DOMContentLoaded", loadQuizQuestion, false);
 
 // set up question counts
 console.log("no of questions: " + questions.length)
 let questionCount = 0;
 let correctCount = 0;
+
+const quiz = document.getElementById("quiz");
+
+function loadQuizQuestion() {
+    quiz.innerHTML = `
+        
+            <h3 id="questionTitle">${questions[questionCount].question}</h3>
+            <p>Question: ${questionCount+1}/${questions.length}</p>
+            
+            <div id="answerSet">
+                <div class="answerBox" onclick="selectedAnswer(this)">
+                    <input type="radio" id="a" name="disasterQuiz" value="${questions[questionCount].options[0]}">
+                    <label for="a">${questions[questionCount].options[0]}</label>
+                </div>
+                <div class="answerBox" onclick="selectedAnswer(this)">
+                    <input type="radio" id="b" name="disasterQuiz" value="${questions[questionCount].options[1]}">
+                    <label for="b">${questions[questionCount].options[1]}</label>
+                </div>
+                <div class="answerBox" onclick="selectedAnswer(this)">
+                    <input type="radio" id="c" name="disasterQuiz" value="${questions[questionCount].options[2]}">
+                    <label for="c">${questions[questionCount].options[2]}</label>
+                </div>
+                <div class="answerBox" onclick="selectedAnswer(this)">
+                    <input type="radio" id="d" name="disasterQuiz" value="${questions[questionCount].options[3]}">
+                    <label for="d">${questions[questionCount].options[3]}</label>
+                </div>
+                <p class="quizError">Please choose an answer</p>
+                <button type="button" onclick="myFunction()" id="quizSubmit" class="optionSubmit">Submit</button>
+
+            </div>
+        
+        `;               
+}
+
+window.addEventListener("DOMContentLoaded", loadQuizQuestion, false);
+
+
 
 // submit button - to next question or submit final answers
 function myFunction() {
@@ -76,34 +108,79 @@ function myFunction() {
 
         // check if selected answer is correct
         const userAnswer = document.querySelector(`input[name='disasterQuiz']:checked`).id;
-        console.log(userAnswer);
+        const correctAnswer = questions[questionCount].answer;
+        const userCorrect = (userAnswer == correctAnswer);
+        console.log(userAnswer, correctAnswer);
+        console.log(userCorrect);
 
-        if (userAnswer == questions[questionCount].answer) {
+        if (userAnswer == correctAnswer) {
             correctCount++;
             console.log("Correct");
         } else {
             console.log("Incorrect");
         }
 
-        // turn submit button into a continue button, show correct answer until btn clicked
+        // disable submit button until timer begins and ends
+        document.getElementById("quizSubmit").disabled = true;
 
+        // turn wrong answers red
+        const answerBoxes = document.querySelectorAll(".answerBox");
+        answerBoxes.forEach(box => {
+            box.classList.add('wrong');
+        });
 
-        // load next question
-        if (questionCount+1 < questions.length) {
-            questionCount++;
-            loadQuizQuestion();
-                
+        // remove wrong class from correct answer, turn green
+        const r = document.getElementById(correctAnswer);
+        const parent = r.closest(".answerBox");
+        parent.classList.remove("wrong");
+        parent.classList.add("correct");
+        
+        
+        // continue, wait for 4 seconds to see correct answer before next answer loads
+        setTimeout(() => {
+            console.log("Hello, its good to be back");
 
-        }
+            // load next question if qcount < qlength
+            if (questionCount+1 < questions.length) {
+                questionCount++;
+                loadQuizQuestion();
+
+            } else {
+                // load results screen if no more questions
+                console.log("Quiz finished");
+                loadResults();
+            }
+        }, 2000);
+    
+
+        
     } else {
         // display error message
         // -> if user clicks on radio after message, make it disappear
         console.log("No answer selected.")
     }
 
+}
 
+
+function selectedAnswer(selectedBox) {
+    // change class of options if one is selected
+    const answerBoxes = document.querySelectorAll(".answerBox");
     
+    // remove selected class from all boxes
+    answerBoxes.forEach(box => {
+        box.classList.remove('selected');
+    })
+    
+    // add selected class to selected box
+    selectedBox.classList.add("selected"); 
+}
 
+function loadResults() {
+    quiz.innerHTML = `
+        <h3>results</h3>
+        <p>${correctCount} / ${questions.length}</p>
+    `;
 }
 
 
